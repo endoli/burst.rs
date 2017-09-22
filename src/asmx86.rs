5f0f60edb9104d33a940abff60e34f4dd1aab0ac
Bruce Mitchener <bruce.mitchener@gmail.com>
2017-09-22 17:41:33 +0700

    Add license header to corroded code.

--- src/asmx86.rs
+++ src/asmx86.rs
@@ -1,3 +1,8 @@
+// Licensed under the 2-Clause BSD license <LICENSE or
+// https://opensource.org/licenses/BSD-2-Clause>. This
+// file may not be copied, modified, or distributed
+// except according to those terms.
+
 #![allow(non_camel_case_types,
          non_snake_case)]
 
