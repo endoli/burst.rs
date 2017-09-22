8d6d0c05fad8c0dcf374d1dcad663dd05691c137
Bruce Mitchener <bruce.mitchener@gmail.com>
2017-09-22 13:11:03 +0700

    Stub initial commit before adding asmx86.

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+// Licensed under the 2-Clause BSD license <LICENSE or
+// https://opensource.org/licenses/BSD-2-Clause>. This
+// file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! # Burst
