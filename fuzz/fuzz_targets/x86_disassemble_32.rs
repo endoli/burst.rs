75439390e5813055d00d010abd74189ac4320099
Bruce Mitchener <bruce.mitchener@gmail.com>
2017-10-14 22:36:41 +0700

    Add additional fuzz testing.

---
+++ fuzz/fuzz_targets/x86_disassemble_32.rs
@@ -0,0 +1,13 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate burst;
+
+use burst::x86::*;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(instr) = disassemble_32(data, 0, data.len()) {
+        let mut out = String::new();
+        format_instruction_string(&mut out, "%a %b %i %o", Some(data), 0, &instr).unwrap();
+    };
+});
